446e3bfe9156d00abe7ec87c2d304ab67c164732
Mike Nachbaur <mike.nachbaur@okta.com>
2021-06-03 15:58:00 -0700

    Initial import

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OktaSdk",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "OktaSdk", targets: [ "OktaSdk" ])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Flight-School/AnyCodable", .exact("0.4.0"))
+    ],
+    targets: [
+        .target(name: "OktaSdk",
+            dependencies: [
+                .product(name: "AnyCodable", package: "AnyCodable")
+            ])
+    ]
+)
---
+++ Sources/OktaSdk/OktaClient.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mike Nachbaur on 2021-06-03.
+//
+
+import Foundation
+
+public class OktaClient: OpenAPIClientAPI {
+    
+}
