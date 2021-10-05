7efce3473e17bfc82edfbc01cc72a648422ad37b
Mike Nachbaur <mike.nachbaur@okta.com>
2021-10-04 17:22:22 -0700

    Update copyright headers

--- Sources/OktaSdk/OktaClient+Network.swift
+++ Sources/OktaSdk/OktaClient+Network.swift
@@ -1,8 +1,13 @@
 //
-//  OktaClient+Network.swift
-//  OktaSdk
+// Copyright (c) 2021-Present, Okta, Inc. and/or its affiliates. All rights reserved.
+// The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
 //
-//  Created by Mike Nachbaur on 2021-09-17.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and limitations under the License.
 //
 
 import Foundation
